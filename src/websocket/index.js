cba3e012a1469edc390596d78877a451f55e9820
Ian <ianmmah@gmail.com>
2020-10-17 20:58:01 -0700

    log only emergency meetings

--- src/websocket/index.js
+++ src/websocket/index.js
@@ -26,8 +26,7 @@ export default () => {
   window.addEventListener(
     'message',
     (event) => {
-      console.log(event)
-      if (event.type === 'from_extension') {
+      if (event.data?.type === 'from_extension') {
         console.log(event)
       }
     },
