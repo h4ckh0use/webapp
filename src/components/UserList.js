fb98b3c6005fe625ff470303ec32e2f770e04e56
Ian <ianmmah@gmail.com>
2020-10-18 06:08:15 -0700

    Avatar

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import Avatar from './Avatar'
+
+const Flexyboi = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
 
 const P = styled.p`
-  margin: 0;
+  margin: 2px 0 0 7px;
 `
 
 export default ({ users }) => {
   return (
     <>
       {Object.entries(users).map((username, color) => {
-        return <P>{username}</P>
+        return (
+          <Flexyboi>
+            <Avatar />
+            <P>{username}</P>
+          </Flexyboi>
+        )
       })}
     </>
   )
