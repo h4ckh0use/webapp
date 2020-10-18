2c72181d411cff40b3c1673c4b5422f97acb00ab
Ian <ianmmah@gmail.com>
2020-10-17 22:06:33 -0700

    style chat box

--- src/containers/ParseWebsocket.js
+++ src/containers/ParseWebsocket.js
@@ -4,7 +4,7 @@ import Logs from '../components/Logs'
 // This component gets the messages from websocket,
 // filters them, parses, and passes them to Logs component
 const ParseWebsocket = ({ ws }) => {
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState(['Welcome to imPomter'])
 
   ws.onmessage = (message) => {
     const data = JSON.parse(message.data)
