a30d198be44a21d5dbfa79b8ed6db0e907fd97ab
Ian <ianmmah@gmail.com>
2020-10-17 20:47:01 -0700

    try to get rid of eslint err

--- src/components/Callroom.js
+++ src/components/Callroom.js
@@ -3,11 +3,7 @@ import { useLocation } from 'react-router-dom'
 
 const Callroom = (props) => {
   const location = useLocation()
-  return (
-    // The <> tag is just a shorthand for React.fragment
-    // https://reactjs.org/docs/fragments.html
-    <>Hey There {location.state.name}</>
-  )
+  return <>Hey There {location.state.name}</>
 }
 
 export default Callroom
